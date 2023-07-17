5f46dbdc8e95c9072fc97a014f144c328ad70ff2
AlessandraCampos <alessandra.campos1983@gmail.com>
2023-07-17 01:06:53 -0300

    Inclusoes

--- 01 - Estrutura de dados/03 - Conjuntos/00_declarando_conjuntos.py
+++ 01 - Estrutura de dados/03 - Conjuntos/00_declarando_conjuntos.py
@@ -6,3 +6,12 @@ print(letras)  # {"b", "a", "c", "x", "i"}
 
 carros = set(("palio", "gol", "celta", "palio"))
 print(carros)  # {"gol", "celta", "palio"}
+
+carros2= {"palio", "gol", "celta", "palio"}
+print(f' Este é o carros {carros2}')
+for indice,carro in enumerate(carros2):
+    print(f' carro - {indice} {carro}')
+
+lista_carro = list(carros2)
+
+print(lista_carro[2])
